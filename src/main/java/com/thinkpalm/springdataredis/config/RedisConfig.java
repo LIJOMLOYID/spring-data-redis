c3a32bdb733375c67c1e31b14e567891378bf0f3
LIJOMLOYID <lijomloyid@gmail.com>
2022-07-01 16:39:23 +0530

    feat: create redis template through which we can access redis server

--- src/main/java/com/thinkpalm/springdataredis/config/RedisConfig.java
+++ src/main/java/com/thinkpalm/springdataredis/config/RedisConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author Lijo M Loyid
@@ -24,5 +27,18 @@ public class RedisConfig {
 		
 		
 	}
-
+	
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory());
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+		template.setValueSerializer(new JdkSerializationRedisSerializer());
+		template.setEnableTransactionSupport(true);
+		template.afterPropertiesSet();
+		return template;
+		
+	}
 }
